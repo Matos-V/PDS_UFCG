696088c016821901884ef7d49a7e03bf3b946026
Débora Oliveira <debora.oliveira@ee.ufcg.edu.br>
2020-09-25 00:57:27 -0300

    Adicionando códigos no MATLAB (Q1-Q4) e doc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PDS_UFCG
-Repositório criado com a finalidade de disponibilizar códigos para os exercícios da disciplina Processamento Digital de Sinais, da UFCG
+# Projeto 1
+Código referente as questões 1 a 4 do projeto 1
---
+++ matlab/amostragem.m
@@ -0,0 +1,78 @@
+rng('default');
+
+%Global parameters
+Fs = 6000;
+Ts = 0:1/Fs:1;
+
+%Continous ploting
+F = 1e4;
+t = 0:1/F:1;
+%%
+%Sampled function  
+xHandle =  @(t) cos(2*pi*3200*t) ...
+            + 0.5*cos(2*pi*600*t) ...
+            + 0.01*cos(2*pi*300*t);
+xContinuous = xHandle(t);
+x = xHandle(Ts);
+%%
+%Plotting
+figure();
+fplot(xHandle,"LineWidth",1.5,'Color',[0.985 0.727 0.258])
+hold on
+stem(Ts,x,"filled");
+xlim([0 0.01]); grid on;
+xticks([0 0.002 0.004 0.006 0.008 0.01])
+xlabel("t [s]", "Interpreter","latex")
+
+legend("Sinal contínuo","Amostrado","Location","northoutside",...
+    "Orientation","horizontal")
+%%
+%FFT
+y = fft(x);                               
+n = length(x);      
+
+%double sided FFT
+y0 = fftshift(y);
+
+m = abs(y0); 
+f = (-n/2:n/2-1)*(Fs/n); 
+
+%FFT continuous
+yC = fft(xContinuous);                               
+nC = length(xContinuous);      
+
+%double sided FFT
+y0C = fftshift(yC);
+
+mC = abs(y0C); 
+fC = (-nC/2:nC/2-1)*(F/nC); 
+%%
+figure();
+plot(fC,mC,"LineWidth",2), hold on,
+plot(f,m,"LineWidth",2)
+ylabel('Magnitude')
+xlabel('Frequency (Hz)'),
+grid;
+legend("Sinal contínuo", "Amostrado","Location","northoutside","Orientation","horizontal")
+xlim([-5000 5000])
+%xlim([-2*pi 2*pi])
+%xticks([-2*pi -1.5*pi -pi -0.5*pi 0.0 0.5*pi pi 1.5*pi 2*pi]);
+%xticklabels({'-2\pi','-1.5\pi','-\pi','-0.5\pi','0.0','0.5\pi','\pi','1.5\pi','2\pi'});
+%%
+figure();
+fplot(xHandle,"LineWidth",1.5,'Color',[0.985 0.727 0.258]); hold on
+stem(Ts,x,"filled");
+xlim([0 0.01]); grid on;
+xticks([0 0.002 0.004 0.006 0.008 0.01])
+xlabel("t [s]", "Interpreter","latex")
+
+%s = interp1(Ts,x,t,'spline');
+
+interp = 0;
+for i = 1:length(Ts)
+    interp = interp + x(i)*sinc((t-(i-1)/Fs)*Fs);
+end
+
+stem(t,interp,'k');
+legend("Sinal contínuo","Amostrado","Recuperado","Location","northoutside",...
+    "Orientation","horizontal")
